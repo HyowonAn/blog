698094897e18fd5c7e64faef285af15fd8a1f6a1
uPrin <503109@naver.com>
2019-08-06 23:20:28 +0900

    Added previewPane Func
    Added previewPane Func

--- blog-frontend/src/pages/EditorPage.js
+++ blog-frontend/src/pages/EditorPage.js
@@ -3,13 +3,14 @@ import EditorTemplate from 'components/editor/EditorTemplate'
 import EditorHeader from 'components/editor/EditorHeader';
 import EditorPaneContainer from 'containers/editor/EditorPaneContainer';
 import PreviewPane from 'components/editor/PreviewPane';
+import PreviewPaneContainer from 'containers/editor/PreviewPaneContainer';
 
 const EditorPage = () => {
     return (
         <EditorTemplate
             header={<EditorHeader/>}
             editor={<EditorPaneContainer/>}
-            preview={<PreviewPane/>}
+            preview={<PreviewPaneContainer/>}
         />
     );
 };
