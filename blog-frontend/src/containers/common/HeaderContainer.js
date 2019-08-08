0dbe00cf4d6a0db65328416036c5e70da0cb06bf
uPrin <503109@naver.com>
2019-08-09 00:15:03 +0900

    Added Modal Func with Edit/Delete Post

--- blog-frontend/src/containers/common/HeaderContainer.js
+++ blog-frontend/src/containers/common/HeaderContainer.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
 import Header from 'components/common/Header';
 import {withRouter} from 'react-router-dom';
+import * as baseActions from 'store/modules/base';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
 
 
 class HeaderContainer extends Component {
     handleRemove = () => {
-        // initial create
+        const {BaseActions} = this.props;
+        BaseActions.showModal('remove');
     }
 
     render() {
@@ -24,4 +28,9 @@ class HeaderContainer extends Component {
 }
 
 
-export default withRouter(HeaderContainer);
+export default connect(
+    (state) => ({}),
+    (dispatch) => ({
+        BaseActions: bindActionCreators(baseActions, dispatch)
+    })
+)(withRouter(HeaderContainer));
