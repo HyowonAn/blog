301c0927d20fc3741cdfc4e84dae71066a0cf24a
uPrin <503109@naver.com>
2019-08-09 00:45:15 +0900

    added session at backend via koa-session

--- blog-backend/src/index.js
+++ blog-backend/src/index.js
@@ -5,11 +5,13 @@ const bodyParser = require('koa-bodyparser');
 
 const mongoose = require('mongoose');
 const api = require('./api');
+const session = require('koa-session');
 
 
 const {
     PORT: port = 4000,
-    MONGO_URI: mongoURI
+    MONGO_URI: mongoURI,
+    COOKIE_SIGN_KEY: signKey
 } = process.env;
 
 mongoose.Promise = global.Promise;
@@ -26,6 +28,12 @@ router.use('/api', api.routes());
 app.use(bodyParser());
 app.use(router.routes()).use(router.allowedMethods());
 
+const sessionConfig = {
+    maxAge: 6000*60*24,
+}
+
+app.use(session(sessionConfig, app));
+app.keys = [signKey]
 
 app.listen(port, () => {
     console.log('listening to port', port);
