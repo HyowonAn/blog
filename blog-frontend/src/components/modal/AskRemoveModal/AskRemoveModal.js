a2028e5c814a8ed2cb1a42877affbca974ca574f
uPrin <503109@naver.com>
2019-08-08 15:57:47 +0900

    Added Modal Func
    Added Modal Func

---
+++ blog-frontend/src/components/modal/AskRemoveModal/AskRemoveModal.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from './AskRemoveModal.scss';
+import classNames from 'classnames/bind';
+import ModalWrapper from 'components/modal/ModalWrapper';
+
+
+const cx = classNames.bind(styles);
+
+const AskRemoveModal = () => {
+    return (
+        <ModalWrapper>
+            내용
+        </ModalWrapper>
+    );
+};
+
+export default AskRemoveModal;
